c1d22dc172d43d9f28b5999592c95455737d8922
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-04-02 16:03:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -104,4 +104,4 @@ async def extract_text(image: UploadFile = File(...)):
     return JSONResponse(status_code=200, content={"extractedText": extracted_text})
 
 if __name__ == "__main__":
-   uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
